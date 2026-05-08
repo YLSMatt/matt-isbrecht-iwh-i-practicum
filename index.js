db6e244bf37310fb566deb2bac3c4edb09673b42
YLSMatt <75279077+YLSMatt@users.noreply.github.com>
2026-05-08 16:15:56 +0000

    Added Post routes, switched to new developer account, was getting unexplained errors on the previous account. Post now working

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
+app.set('views', './views');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,15 +14,46 @@ const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { 
+        pageTitle: 'Update Custom Object Form | Integrating With HubSpot I Practicum' 
+    });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const customObjectId = '2-62152219';
+    const url = `https://api.hubapi.com/crm/v3/objects/${customObjectId}`;
+    
+    const newRecord = {
+        properties: {
+            name: req.body.name,
+            origin: req.body.origin,
+            effectiveness: req.body.effectiveness
+        }
+    };
+    
+    const headers = {
+        Authorization: `Bearer ${process.env.HUBSPOT_ACCESS_TOKEN}`,
+        'Content-Type': 'application/json'
+    };
+    
+    try {
+        console.log('Token present:', process.env.HUBSPOT_ACCESS_TOKEN ? 'Yes' : 'No');
+        console.log('Token starts with:', process.env.HUBSPOT_ACCESS_TOKEN?.substring(0, 10));  
+        await axios.post(url, newRecord, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error('Error creating record:', error.response?.data || error.message);
+        res.status(500).send('Error creating record');
+    }
+});
+
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
